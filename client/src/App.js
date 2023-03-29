6481487d20e12a2c845561a9da0260fdb7ab0f76
JuanCruzRausch <juancruz_rausch@hotmail.com>
2023-03-29 20:52:03 -0300

    footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>NavBar</h1>
+      <h1>Imagen Con Boton</h1>
+      <h1>Tarjetas</h1>
+      <h1>Info</h1>
+      <h1>About Us</h1>
+      <h1>Call to action</h1>
+      <h1>Buy Now</h1>
+      <h1>Carousel</h1>
+      <h1>Redes Sociales</h1>
+      <Footer />
     </div>
   );
 }
