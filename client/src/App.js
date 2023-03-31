6c14595c9b7ae931333618540550675205d5ce03
Angel-jr <angelfp28@gmail.com>
2023-03-31 05:26:21 -0300

    Angeljr

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
-import Navbar from './componentes/Header/NavBar';
-
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Header"
 function App() {
   return (
     <div className='App'>
-      <div>
-            <Navbar/>
-        </div>
+        <Navbar/>
+        <h1>Imagen Con Boton</h1>
+        <h1>Tarjetas</h1>
+        <h1>Info</h1>
+        <h1>About Us</h1>
+        <h1>Call to action</h1>
+        <h1>Buy Now</h1>
+        <h1>Carousel</h1>
+        <h1>Redes Sociales</h1>
+        <Footer/>
     </div>
   );
 }
-
 export default App;
